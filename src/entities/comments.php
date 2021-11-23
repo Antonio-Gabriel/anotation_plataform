1a0d3fb0e274047a61e6f8000d05da09a7f68471
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-11-23 18:29:24 +0100

    comments entiti

---
+++ src/entities/comments.php
@@ -0,0 +1,10 @@
+<?php
+
+class Comments {
+    
+    public int $idComment;
+    public string $attr_title;
+    public string $attr_description;
+    public datetime $created_at;
+    public datetime $updated_at;
+}
